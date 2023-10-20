8c35700a24c4db640f0564051d547393dbea4c1c
Caninas <pgcaninas@gmail.com>
2023-10-19 21:50:01 -0300

    base substituição

--- Analisador Sintatico.py
+++ Analisador Sintatico.py
@@ -1,12 +1,29 @@
 import re
 
+s = "def abc() {\n}"
 
-s = "def abc() {"
-""
-"}"
+s = open("./teste.txt", "r").read()
 
+palavras_reservadas = {"def", "int"}
 
-x = re.findall("\w*\S[^()]", s)
-#re.sub
+# texto_final = ""
+# def seila2(matchobj):
+#     global texto_final
+#     if matchobj.group(0) in palavras_reservadas or re.match("\w+\d", matchobj.group(0)):
+#         texto_final += "IDENT"
+    
+#     texto_final += "OUTRO"
+
+def substituir(matchobj):
+    if matchobj.group(0) in palavras_reservadas or re.match("\w+\d", matchobj.group(0)):
+        return "IDENT"
+    
+    return "OUTRO"
+
+comp = re.compile("\w+|\S", re.MULTILINE)
+x = re.findall(comp, s)
+
+j = re.sub(comp, substituir, s)
 
 print(x)
+print(j)
